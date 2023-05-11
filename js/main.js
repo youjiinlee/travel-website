d4becf56159abe14b3dfa7923ee78a5578f6da34
youjiinlee <132879740+youjiinlee@users.noreply.github.com>
2023-05-11 16:46:41 +0900

    travel website
    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+
+    autoplay: {
+      delay: 2500,
+    },
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
